0f1b57eba565ee8e94192e87e51d0ed2eb7bf5c7
unknown <youssef.ojeil@hotmail.com>
2023-01-18 18:46:04 -0500

    randomipfsnft contract and deploy mocks script

--- hardhat/contracts/RandomIpfsNft.sol
+++ hardhat/contracts/RandomIpfsNft.sol
@@ -9,7 +9,18 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "hardhat/console.sol";
 
-contract RandomIpfsNft is VRFConsumerBaseV2, ERC721 {
+error RandomIpfsNft__RangeOutOfBounds();
+error RandomIpfsNft__NeedMoreEthSent();
+error RandomIpfsNft__TransferFailed();
+
+contract RandomIpfsNft is VRFConsumerBaseV2, ERC721URIStorage, Ownable {
+
+    // Type Declarations
+    enum Breed {
+        PUG,
+        SHIBA_INU,
+        ST_BERNARD
+    }
 
     // Chainlink VRF Variables
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
@@ -24,20 +35,37 @@ contract RandomIpfsNft is VRFConsumerBaseV2, ERC721 {
 
     // NFT Variables
     uint256 public s_tokenCounter;
+    uint256 internal constant MAX_CHANCE_VALUE = 100; 
+    string[] internal s_dogTokenUris;
+    uint256 internal immutable i_mintFee;
 
-    constructor(address vrfCoordinatorV2, uint64 subscriptionId, bytes32 gasLane, uint32 callbackGasLimit ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random IPFS NFT", "RIN") {
+    // Events
+    event NftRequested(uint256 indexed requestId, address requester);
+    event NftMinted(Breed dogBreed, address minter);
+
+
+    constructor(address vrfCoordinatorV2, uint64 subscriptionId, bytes32 gasLane, 
+    uint32 callbackGasLimit, string[3] memory dogTokenUris, uint256 mintFee ) 
+    VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random IPFS NFT", "RIN") {
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_subscriptionId = subscriptionId;
         i_gasLane = gasLane;
         i_callbackGasLimit = callbackGasLimit;
+        s_dogTokenUris = dogTokenUris;
+        i_mintFee = mintFee;
     }
 
     // create mapping between requestid and whoever calls this function
-    function requestNft() public returns (uint256 requestId) {
+    function requestNft() public payable returns (uint256 requestId) {
+        if(msg.value < i_mintFee) {
+            revert RandomIpfsNft__NeedMoreEthSent();
+        }
         requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane, i_subscriptionId, REQUEST_CONFIRMATIONS, i_callbackGasLimit, NUM_WORDS
         );
         s_requestIdToSender[requestId] = msg.sender;
+
+        emit NftRequested(requestId, msg.sender);
     }
 
     // this is called by the chainlink node 
@@ -45,9 +73,53 @@ contract RandomIpfsNft is VRFConsumerBaseV2, ERC721 {
 
         address dogOwner = s_requestIdToSender[requestId];
         uint256 newTokenId = s_tokenCounter;
-        _safeMint(dogOwner, newTokenId);
 
+        uint256 moddedRng = randomWords[0] % MAX_CHANCE_VALUE;
+
+         Breed dogBreed = getBreedFromModdedRng(moddedRng);
+        _safeMint(dogOwner, newTokenId);
+        _setTokenURI(newTokenId, s_dogTokenUris[uint256(dogBreed)]);
+        emit NftMinted(dogBreed, dogOwner);
     }
 
-    function tokenURI(uint256) public view override returns (string memory){}
+    function withdraw() public onlyOwner {
+        uint256 amount = address(this).balance;
+        (bool success,) = payable(msg.sender).call{value: amount}("");
+        if(!success) {
+            revert RandomIpfsNft__TransferFailed();
+        }
+    }
+
+    function getBreedFromModdedRng(uint256 moddedRng) public pure returns (Breed) {
+        uint256 cummulativeSum = 0;
+        uint256[3] memory chanceArray = getChanceArray();
+        // moddedRng = 25
+        // i = 0
+        // cummulativeSum = 0
+        for(uint256 i =0; i < chanceArray.length; i ++) {
+            if(moddedRng >= cummulativeSum && moddedRng < cummulativeSum + chanceArray[i]) {
+                return Breed(i);
+            }
+            cummulativeSum += chanceArray[i];
+        }
+        revert RandomIpfsNft__RangeOutOfBounds();
+    }
+
+    function getChanceArray() public pure returns(uint256[3] memory) {
+        return [10, 30, MAX_CHANCE_VALUE];
+    }
+
+    function getMintFee() public view returns (uint256) {
+        return i_mintFee;
+    }
+
+    function getDogTokenUris(uint256 index) public view returns (string memory) {
+        return s_dogTokenUris[index];
+    }
+
+    function getTokenCounter() public view returns(uint256) {
+        return s_tokenCounter;
+    }
+
+
 }
