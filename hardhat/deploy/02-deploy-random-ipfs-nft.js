c13e6ef9ed5afce1ac60c62ca47bcf31e234e478
unknown <youssef.ojeil@hotmail.com>
2023-01-19 22:28:31 -0500

    install pinata, import images and setup pinata ipfs upload

---
+++ hardhat/deploy/02-deploy-random-ipfs-nft.js
@@ -0,0 +1,54 @@
+const { network, ethers } = require("hardhat");
+const {
+  developmentChains,
+  networkConfig,
+} = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+  let tokenUris;
+  // before deploying contract
+  // get ipfs hashes of images
+  if (process.env.UPLOAD_TO_PINATA == "true") {
+    tokenUris = await handleTokenUris();
+  }
+
+  // pinata or nft.storage
+
+  let vrfCoordinatorV2Address, subscriptionId;
+
+  if (developmentChains.includes(network.name)) {
+    const vrfCoordinatorV2Mock = await ethers.getContract(
+      "VRFCoordinatorV2Mock"
+    );
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+    const tx = await vrfCoordinatorV2Mock.createSubscription();
+    const txReceipt = await tx.wait(1);
+    subscriptionId = txReceipt.events[0].args.subId;
+  } else {
+    vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2;
+    subscriptionId = networkConfig[chainId].subscriptionId;
+  }
+
+  log("----------------------------------------------");
+
+  const args = [
+    vrfCoordinatorV2Address,
+    subscriptionId,
+    networkConfig[chainId].gasLane,
+    networkConfig[chainId].mintFee,
+    networkConfig[chainId].callbackGasLimit,
+  ];
+};
+
+async function handleTokenUris() {
+  tokenUris = [];
+
+  //store the image in ipfs
+  // store metadata in ipfs
+
+  return tokenUris;
+}
